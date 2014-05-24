860f9fd8b9c1f991731dc6b1e347b43c5f5e7fdb
Quim Llimona <ql@lemonzi.me>
2014-05-24 12:55:09 -0400

    basic layout from API's:

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 var osc           = require('node-osc');
 var spheroFactory = require('node-sphero');
+var config        = require('./config');
 
-var oscClient = new osc.Client('127.0.0.1', 3333);
-oscClient.send('/oscAddress', 200);
-
-var oscServer = new osc.Server(3333, '0.0.0.0');
-oscServer.on("message", function (msg, rinfo) {
-    console.log("TUIO message:");
-    console.log(msg);
-});
+var oscClient = new osc.Client('127.0.0.1', config['osc-send']);
+var oscServer = new osc.Server(config['osc-receive'], '0.0.0.0');
 
 var sphero = new spheroFactory.Sphero();
 
 sphero.on('connected', function(ball) {
-    ball.setRGBLED(0, 255, 0, false);
+
+    // Control Sphero from OSC
+    oscServer.on('message', function (msg, rinfo) {
+        // Parse message and send it to Sphero
+        ball.setRGBLED(0, 255, 0, false);
+    });
+
+    // Set up the sensor callbacks
+    ball.setDataStreaming(
+        Object.keys(sphero.sensors),    // Which sensors to read
+        config.frameRate,               // Frame rate (def. 400)
+        frames || 0,                    // What is this??
+        count || 0                      // What is this??
+    );
+
+    ball.on('message', function(msg) {
+        // Parse data and send it via OSC
+        oscClient.send('/oscAddress', msg.DATA);
+    });
+
 });
 
+// Run!
 sphero.connect();
