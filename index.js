8552c9dd57ce4264210f1127ea3696e5c404640f
Quim Llimona <ql@lemonzi.me>
2014-05-25 15:21:06 -0400

    It works

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-var osc    = require('node-osc');
-var sphero = require('spheron').sphero();
-var config = require('./config');
+var osc     = require('node-osc');
+var spheron = require('spheron');
+var config  = require('./config');
+
+var sphero   = spheron.sphero();
+var commands = spheron.commands;
 
 var oscClient = new osc.Client('127.0.0.1', config['osc-send']);
 var oscServer = new osc.Server(config['osc-receive'], '0.0.0.0');
@@ -8,9 +11,12 @@ var oscServer = new osc.Server(config['osc-receive'], '0.0.0.0');
 sphero.on('open', function() {
     console.log('Connected!');
 
+    // Turn off the motors
+    sphero.write(commands.api.setStabilisation(false));
+
     // Start streaming sensors
-    var mask = 0x000007FE7;
-    var rate = 400 / config.rate;
+    var mask = 0x0007FC00;
+    var rate = Math.round(400 / config.frameRate);
     sphero.setDataStreaming(rate, 1, mask, 0);
 
 });
@@ -23,15 +29,31 @@ oscServer.on('message', function (msg, rinfo) {
 
 sphero.on('packet', function(packet) {
     // Parse data and send it via OSC
-    console.log(packet);
+    // console.log(packet);
     if (packet.ID_CODE === 0x03) {
-        console.log({
-            x: packet.DATA.readInt16BE(0),
-            y: packet.DATA.readInt16BE(2),
-            z: packet.DATA.readInt16BE(4),
-        });
+        var data = [
+            packet.DATA.readInt16BE(0),
+            packet.DATA.readInt16BE(2),
+            packet.DATA.readInt16BE(4),
+            packet.DATA.readInt16BE(6),
+            packet.DATA.readInt16BE(8),
+            packet.DATA.readInt16BE(10),
+            packet.DATA.readInt16BE(12),
+            packet.DATA.readInt16BE(14),
+            packet.DATA.readInt16BE(16),
+        ];
+        console.log(data);
+        // oscClient.send('/sphero/1/raw', data);
+        oscClient.send('/sphero/1/accel/x',      +(data[0]));
+        oscClient.send('/sphero/1/accel/y',      +(data[1]));
+        oscClient.send('/sphero/1/accel/z',      +(data[2]));
+        oscClient.send('/sphero/1/angles/pitch', +(data[3]));
+        oscClient.send('/sphero/1/angles/roll',  +(data[4]));
+        oscClient.send('/sphero/1/angles/yaw',   +(data[5]));
+        oscClient.send('/sphero/1/gyro/x',       +(data[6]));
+        oscClient.send('/sphero/1/gyro/y',       +(data[7]));
+        oscClient.send('/sphero/1/gyro/z',       +(data[8]));
     }
-    oscClient.send('/oscAddress', packet.DATA);
 });
 
 // Run!
